08cde9e7b0c19671211a32b94a9ce0571685981b
Ryan <ryyu444@gmail.com>
2025-04-27 20:09:41 -0700

    added skeleton test

--- HW2/frontend/__tests__/api/api.test.ts
+++ HW2/frontend/__tests__/api/api.test.ts
@@ -1,15 +1,23 @@
 import { describe, expect, it } from '@jest/globals';
-import getAPIKey from "../../src/lib/helpers/getAPIKey";
-import getArticles from "../../src/lib/helpers/getArticles";
+import getAPIKey from '../../src/lib/helpers/getAPIKey';
+import getArticles from '../../src/lib/helpers/getArticles';
 
 // want to mock our calls to fetch the NYT API key
-describe('API Tests', () => {
-  it('should add two numbers', () => {
+describe('NYT API Key', () => {
+  it('should fetch our NYT API Key from the flask backend', () => {
     const a = 5;
     const b = 10;
     const sum = a + b;
     expect(sum).toBe(15);
   });
 });
-// want to mock our calls to fetch data from the NYT API
 
+// want to mock our calls to fetch data from the NYT API
+describe('NYT API', () => {
+  it('should fetch articles from the NYT API', () => {
+    const a = 5;
+    const b = 10;
+    const sum = a + b;
+    expect(sum).toBe(15);
+  });
+});
