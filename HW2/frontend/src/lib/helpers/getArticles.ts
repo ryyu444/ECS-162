9c0d3c3e97e8a0160fc3170d11dafcb37277c25e
Ryan <ryyu444@gmail.com>
2025-04-30 16:17:54 -0700

    modified query for NYT API

--- HW2/frontend/src/lib/helpers/getArticles.ts
+++ HW2/frontend/src/lib/helpers/getArticles.ts
@@ -1,5 +1,5 @@
 export default async function getArticles(API_KEY: string) {
-    const queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22UC%20Davis%22%20OR%20%22University%20of%20California%2C%20Davis%22&begin_date=20240101&end_date=20250426&api-key=${API_KEY}`;
+    const queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q="University of California, Davis"&begin_date=20200101&end_date=20250426&api-key=${API_KEY}`;
     const data = await fetch(queryURL).then((res) => res.json());
     console.log(data);
 
